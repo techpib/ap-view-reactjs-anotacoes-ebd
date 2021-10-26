ff376c1a7760857c6f4383e650d977edf02bc34d
augustoluiz <augustogu7@yahoo.com.br>
2021-10-25 23:40:55 -0300

    Estrutura Inicial do Projeto

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import './assets/App.css';
+import './assets/index.css';
+
+class App extends Component {
+
+  render(){
+    return (
+      <section>
+        <h1>Teste</h1>
+      </section>
+    );
+  }
+
+}
+
+export default App;
